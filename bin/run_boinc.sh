6a6a47c1e47cf5b311fc2d04c20ad7dbdd28946a
Vojtěch Hauser <hauser@hauser-ux430.home>
2018-01-14 14:41:43 +0100

    added infinite loop so that the command doesnt end

---
+++ bin/run_boinc.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export BOINC_PORT=${BOINC_PORT:-"3333"}
+
+boinc --allow_multiple_clients --gui_rpc_port $BOINC_PORT &
+
+while [ ! -f "gui_rpc_auth.cfg" ]; do
+   sleep 1
+done
+
+echo "Started BOINC with port ${BOINC_PORT}, password $(cat gui_rpc_auth.cfg)."
+
+while :; do sleep 86400; done
